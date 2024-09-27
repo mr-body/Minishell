2f18285a1dc407f690d03b536e454d35ebcdd209
Walter Alexandre <waalexan@c1r10s9.42luanda.com>
2024-09-27 09:02:29 +0100

    "Tabs

--- minishell.c
+++ minishell.c
@@ -1,13 +1,13 @@
 #include "minishell.h"
 
-t_minishell g_minishell;
-
 static void ctrlC(int signal)
 {
+    t_minishell minishell;
+
     ft_putstr_fd(VERDE "\n┌──" RESET, 1);
     ft_putstr_fd(AZUL "(Minishell)" RESET, 1); 
     ft_putstr_fd(VERDE "-[" RESET, 1);
-    ft_putstr_fd(g_minishell.dir, 1);
+    ft_putstr_fd(minishell.dir, 1);
     ft_putstr_fd(VERDE "]\n", 1);
     rl_on_new_line();
     rl_replace_line("", 0);
@@ -16,10 +16,11 @@ static void ctrlC(int signal)
 
 int main(int ac, char **av)
 {   
-    g_minishell.dir = getcwd(NULL, 0);
+    t_minishell minishell;
 
+    minishell.dir = getcwd(NULL, 0);
     header();
     signal(SIGINT, &ctrlC);
-    command(&g_minishell);
+    command(&minishell);
     return 0;
 }
