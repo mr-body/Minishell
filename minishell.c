79a4831f9bed2b192d8f7c4a0bc836d997f374f0
Gildo Komba <gkomba@c1r11s10.42luanda.com>
2024-10-02 11:49:35 +0100

    teste

--- minishell.c
+++ minishell.c
@@ -2,16 +2,12 @@
 
 static void ctrlC(int signal)
 {
-<<<<<<< HEAD
     t_minishell g_minishell;
-=======
-    t_minishell minishell;
->>>>>>> 34ad2b5e04f5e87a3cc52ef1a3f0f888790bf37d
 
     ft_putstr_fd(VERDE "\n┌──" RESET, 1);
     ft_putstr_fd(AZUL "(Minishell)" RESET, 1); 
     ft_putstr_fd(VERDE "-[" RESET, 1);
-    ft_putstr_fd(minishell.dir, 1);
+    ft_putstr_fd(g_minishell.dir, 1);
     ft_putstr_fd(VERDE "]\n", 1);
     rl_on_new_line();
     rl_replace_line("", 0);
@@ -25,6 +21,6 @@ int main(int ac, char **av)
     g_minishell.dir = getcwd(NULL, 0);
     header();
     signal(SIGINT, &ctrlC);
-    command(&minishell);
+    command(&g_minishell);
     return 0;
 }
