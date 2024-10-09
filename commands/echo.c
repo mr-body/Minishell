ac6c048de249297600f2027fdac8684f3610afe7
Gildo Komba <gkomba@c1r11s10.42luanda.com>
2024-10-09 08:29:45 +0100

    UPDATE

--- commands/echo.c
+++ commands/echo.c
@@ -7,18 +7,33 @@ int	count_line(char **argv)
 	i = 0;
 }
 
-int	command_echo(char **argv, char *cmd, char *bin)
+
+/*
+tmp: Uma string vazia que será utilizada para construir a linha de comando final, após a expansão das variáveis de ambiente.
+i, j, k: Variáveis inteiras usadas como índices para percorrer os argumentos e caracteres.
+Itera sobre cada argumento passado para a função.
+Itera sobre cada caractere do argumento.
+Se encontrar um '$', busca pelo nome da variável de ambiente e substitui o nome da variável pelo seu valor.
+se o seu valor for encontrado ele sera concatenado em tmp. se nao apenas reorna nul e volta a contar o proximo caracter.
+Concatena os caracteres (ou valores das variáveis) à string tmp.
+*/
+int		command_echo(char **argv, char *cmd, char *bin)
 {
 	extern char **environ;
-	char 	*tmp = ft_strdup("");
-	int 	i;
-	char *arg;
-	char *env_var_name;
-	char *env_var_value;
-	int j;
-	int	k;
+	char		*env_var_name;
+	char		*env_var_value;
+	char 		single_char[2];
+	char 		*tmp;
+	char 		*arg;
+	pid_t 		pid;
+	int			i;
+	int			j;
+	int			k;
+	int			c;
 
+	tmp = ft_strdup("");
 	i = 0;
+	c = 0;
 	while (argv[++i])
 	{
 		arg = argv[i];
@@ -29,34 +44,40 @@ int	command_echo(char **argv, char *cmd, char *bin)
 			{
 				j++;
 				k = j;
-				while (arg[k] && (ft_isalnum(arg[k]) || arg[k] == '_')) 
+				while (arg[k] && (ft_isalnum(arg[k]) || arg[k] == '_'))
 					k++;
-				printf("j ->> %d || k ->>> %d\n", j, k);
 				env_var_name = ft_substr(arg, j, k - j);
 				env_var_value = getenv(env_var_name);
-				
 				if (env_var_value)
 				{
+					char *tmp2 = ft_strtrim(env_var_name, "\" ");
 					tmp = ft_strjoin(tmp, env_var_value);
 				}
 				free(env_var_name);
 				j = k;
 			}
+			else if (arg[j] == '"')
+			{
+				c++;
+				j++;
+			}
 			else
 			{
-				char single_char[2] = {arg[j], '\0'};
+				single_char[0] = arg[j];
+				single_char[1] = '\0';
 				tmp = ft_strjoin(tmp, single_char);
 				j++;
 			}
 		}
 		tmp = ft_strjoin(tmp, " ");
 	}
-	pid_t pid = fork();
+	pid = fork();
 	if (pid == 0)
 	{
-		char *args[] = {bin, tmp, "-c"};
+		char *args[] = {bin, tmp, NULL};
 		if (execve(bin, args, environ) == -1)
 		{
+			free(tmp);
 			perror("error");
 			exit(1);
 		}
