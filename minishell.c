1d2a0c72afee76934f3d664d1f0aea2ccc00d257
waalexan <walteralexandresantanag@gmail.com>
2024-10-12 07:39:44 -0400

    pipe (|)

--- minishell.c
+++ minishell.c
@@ -1,106 +1,46 @@
 #include "minishell.h"
 
-char **ft_adjust_data(char **data)
+static void ctrlC(int signal)
 {
-    int i;
-    int j;
-    char **new_data;
-
-    i = -1;
-    j = 0;
-    new_data = (char **)malloc(sizeof(char *) * (ft_matriz_len(data) + 1));
-    while(data[++i])
-    {
-        if(data[i][0] == '\"' && data[i][ft_strlen(data[i]) - 1] == '\"' && ft_strlen(data[i]) > 1)
-            new_data[j++] = ft_strdup(data[i]);
-        else if(data[i][0] == '\"')
-        {
-            new_data[j] = ft_strdup(data[i]);
-            while(data[i+1] && data[i+1][ft_strlen(data[i + 1]) - 1] != '\"')
-               new_data[j] = ft_strcat(new_data[j], data[++i], ' ');
-            if (data[i+1])
-                new_data[j] = ft_strcat(new_data[j], data[++i], ' ');
-            if (new_data[j][ft_strlen(new_data[j]) - 1] != '\"')
-                return (NULL);
-            j++;
-        }
-        else if(data[i][ft_strlen(data[i]) - 1] == '\"')
-            return (NULL);
-        else
-            new_data[j++] = ft_strdup(data[i]);
-    }
-    return (new_data[j] = '\0', new_data);
-}
-
-char **ft_extended(char **data)
-{
-    int i;
-    int j;
-    char **new_data;
-    char *tmp;
-
-    i = -1;
-    new_data = (char **)malloc(sizeof(char *) * (ft_matriz_len(data) + 1));
-    while (data[++i])
-    {
-        j = 0;
-        tmp = NULL; 
-        data[i] = ft_strtrim(data[i], "\""); 
-        while (data[i][j] && data[i][j] != '$')
-            j++;
-        if (data[i][j] == '$') 
-        {
-            char *env_value = getenv(ft_substr(data[i], j + 1, ft_strlen(data[i]) - j - 1));
-            if (env_value) 
-                tmp = ft_strjoin(ft_substr(data[i], 0, j), env_value);
-            else
-                tmp = ft_strdup(data[i]);
-        }
-        else
-            tmp = ft_strdup(data[i]);
-        new_data[i] = tmp;
-    }
-    new_data[i] = '\0';
-    return new_data;
-}
-
-char **args(char *str)
-{
-    char **raw_data;
-    char **net_data;
-    char **data;
-
-    raw_data = ft_split(str, ' ');
-    net_data = ft_adjust_data(raw_data);
-    if(net_data == NULL)
-    {
-        write(1, "minishell: syntax error: quote\n", 31);
-        exit(1);
-    }
-    else
-    {
-        data = ft_extended(net_data);
-    }
-
-    for (int i = 0; raw_data[i]; i++)
-        free(raw_data[i]);
-    free(raw_data);
-
-    return data;
+    (void)signal;
+    ft_putstr_fd(VERDE "\n┌──" RESET, 1);
+    ft_putstr_fd(AZUL "(Minishell)" RESET, 1); 
+    ft_putstr_fd(VERDE "-[" RESET, 1);
+    ft_putstr_fd(getcwd(NULL, 0), 1);
+    ft_putstr_fd(VERDE "]\n", 1);
+    rl_replace_line("", 0);
+    rl_on_new_line();
+    rl_redisplay();
 }
 
 int main(int ac, char **av)
 {
-    char *promt;
-
-    promt = readline("minishell$ ");
-    add_history(promt); 
-    int i = -1;
-    char **string = args(promt);
-    while(string[++i])
+    char *prompt;
+    int status;
+    
+    header();
+    signal(SIGINT, &ctrlC);
+    while(1)
     {
-        printf("%s\n", string[i]);
+        ft_putstr_fd(VERDE "\n┌──" RESET, 1);
+		ft_putstr_fd(AZUL "(Minishell)" RESET, 1);
+		ft_putstr_fd(VERDE "-[" RESET, 1);
+		ft_putstr_fd(getcwd(NULL, 0), 1);
+		ft_putstr_fd(VERDE "]", 1);
+		prompt = readline(VERDE "\n└─" RESET AZUL "# " RESET);
+        if(!prompt)
+            break;
+        if(*prompt)
+            add_history(prompt);
+        if(ft_strncmp(prompt, "exit", 4) == 0)
+        {
+            free(prompt);
+            exit(0);
+        }
+        else
+            execute_command(prompt);
+        while (waitpid(-1, &status, 0) > 0);
+        free(prompt);
     }
-    free(promt);
     return 0;
 }
