600f464a3bc4b2439adf1c2829faad84ecad276d
Gildo Komba <gkomba@c1r10s5.42luanda.com>
2024-10-14 18:10:04 +0100

    working on memory management

--- command/execial.c
+++ command/execial.c
@@ -1,60 +1,82 @@
 #include "../minishell.h"
 
-int command_cd(char **prompt)
+int	command_cd(char **prompt)
 {
-    if(prompt[1])
-        chdir(ft_strtrim(prompt[1], "\'"));
-    else
-        write(1, "\n", 1);
-    return (0);
+	if (prompt[1])
+	{
+		if (chdir(prompt[1]) != 0)
+		{
+			perror("cd error");
+			return (1);
+		}
+	}
+	return (0);
 }
 
-int command_exit(char **prompt)
+int	command_exit(char **prompt)
 {
-    exit(0);
-    return (0);
+	exit(0);
 }
 
-int command_echo(char **prompt)
+int	command_echo(char **prompt, int pipe)
 {
-    int i;
-    i = 0;
-    while(prompt[++i])
-    {
-        write(1, prompt[i], ft_strlen(prompt[i]));
-        write(1, " ", 1);
-    }
-    write(1, "\n", 1);
-    exit(0);
-    return (0);
+	int	i;
+
+	i = 0;
+	while (prompt[++i])
+	{
+		write(1, prompt[i], ft_strlen(prompt[i]));
+		write(1, " ", 1);
+	}
+	write(1, "\n", 1);
+	if (pipe)
+	{
+		ft_free_matriz(prompt);
+		exit(0);
+	}
+	return (0);
 }
 
-int command_env(char **prompt, char **environ)
+int	command_env(char **prompt, char **environ)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while(environ[++i])
-        write(1, environ[i], ft_strlen(environ[i]));
-    write(1, "\n", 1);
-    exit(0);
-    return (0);
+	i = -1;
+	while (environ[++i])
+	{
+		write(1, environ[i], ft_strlen(environ[i]));
+		write(1, "\n", 1);
+	}
+	return (0);
 }
 
-int command_pwd(char **prompt)
+int	command_pwd(char **prompt, int pipe)
 {
-    write(STDOUT_FILENO, getcwd(NULL, 0), ft_strlen(getcwd(NULL, 0)));
-    write(STDOUT_FILENO, "\n", 1);
-    exit(0);
-    return (0);
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
+	{
+		perror("getcwd error");
+		exit(1);
+	}
+	write(STDOUT_FILENO, cwd, strlen(cwd));
+	write(STDOUT_FILENO, "\n", 1);
+	free(cwd);
+	if (pipe)
+	{
+		ft_free_matriz(prompt);
+		exit(0);
+	}
+	return 1;
 }
 
-int command_export(char **prompt)
+int	command_export(char **prompt)
 {
-    return (0);
+	exit(0);
 }
 
-int command_unset(char **prompt)
+int	command_unset(char **prompt)
 {
-    return (0);
+	exit(0);
 }
--- util.c
+++ util.c
@@ -1,128 +1,120 @@
 #include "minishell.h"
 
-void header(void)
+char	*ft_strcat(char *s1, char *s2, int c)
 {
-    ft_putstr_fd(CLEAR, 1);
-    // ft_putstr_fd( VERDE ".88b  d88. d888888b d8b   db d888888b .d8888. db   db d88888b db      db      \n" RESET, 1);
-    // ft_putstr_fd( VERDE "88'YbdP`88   `88'   888o  88   `88'   88'  YP 88   88 88'     88      88      \n" RESET, 1);
-    // ft_putstr_fd( VERDE "88  88  88    88    88V8o 88    88    `8bo.   88ooo88 88ooooo 88      88      \n" RESET, 1);
-    // ft_putstr_fd( VERDE "88  88  88    88    88 V8o88    88      `Y8b. 88~~~88 88~~~~~ 88      88      \n" RESET, 1);
-    // ft_putstr_fd( VERDE "88  88  88   .88.   88  V888   .88.   db   8D 88   88 88.     88booo. 88booo. \n" RESET, 1);
-    // ft_putstr_fd( VERDE "YP  YP  YP Y888888P VP   V8P Y888888P `8888Y' YP   YP Y88888P Y88888P Y88888P \n" RESET, 1);
-    // ft_putstr_fd("", 1);
-                                                                                                                                                         
+	size_t	len1;
+	size_t	len2;
+	char	*new;
+	size_t	i;
+    size_t  j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	new = malloc(len1 + len2 + 2);
+	if (new == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < len1)
+		new[i] = s1[i];
+	new[len1] = (char)c;
+	i = len1 + 1;
+	j = -1;
+	while (++j < len2)
+		new[i++] = s2[j];
+	new[i] = '\0';
+	return (new);
 }
 
-char *ft_strcat(char *s1, char *s2, int c)
+char	**ft_adjust_data(char **data)
 {
-    int i;
-    int j;
-    char *new;
+	int		i;
+	int		j;
+	char	**new_data;
 
-    i = 0;
-    j = 0;
-    new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-    while (s1[i])
-    {
-        new[i] = s1[i];
-        i++;
-    }  
-    new[i++] = c;
-    while(s2[j])
-    {
-        new[i] = s2[j];
-        i++;
-        j++;
-    }
-    new[i] = '\0';
-    return new;
+	i = -1;
+	j = 0;
+	new_data = (char **)malloc(sizeof(char *) * (ft_matriz_len(data) + 1));
+	while (data[++i])
+	{
+		if (data[i][0] == '\"' && data[i][ft_strlen(data[i]) - 1] == '\"'
+			&& ft_strlen(data[i]) > 1)
+			new_data[j++] = ft_strdup(data[i]);
+		else if (data[i][0] == '\"')
+		{
+			new_data[j] = ft_strdup(data[i]);
+			while (data[i + 1] && data[i + 1][ft_strlen(data[i + 1])
+				- 1] != '\"')
+				new_data[j] = ft_strcat(new_data[j], data[++i], ' ');
+			if (data[i + 1])
+				new_data[j] = ft_strcat(new_data[j], data[++i], ' ');
+			if (new_data[j][ft_strlen(new_data[j]) - 1] != '\"')
+				return (NULL);
+			j++;
+		}
+		else if (data[i][ft_strlen(data[i]) - 1] == '\"')
+			return (NULL);
+		else
+			new_data[j++] = ft_strdup(data[i]);
+	}
+	return (new_data[j] = '\0', new_data);
 }
 
-char **ft_adjust_data(char **data)
+char	**ft_extended(char **data)
 {
-    int i;
-    int j;
-    char **new_data;
+	int		i;
+	int		j;
+	char	**new_data;
+	char	*tmp;
+	char	*env_value;
 
-    i = -1;
-    j = 0;
-    new_data = (char **)malloc(sizeof(char *) * (ft_matriz_len(data) + 1));
-    while(data[++i])
-    {
-        if(data[i][0] == '\"' && data[i][ft_strlen(data[i]) - 1] == '\"' && ft_strlen(data[i]) > 1)
-            new_data[j++] = ft_strdup(data[i]);
-        else if(data[i][0] == '\"')
-        {
-            new_data[j] = ft_strdup(data[i]);
-            while(data[i+1] && data[i+1][ft_strlen(data[i + 1]) - 1] != '\"')
-               new_data[j] = ft_strcat(new_data[j], data[++i], ' ');
-            if (data[i+1])
-                new_data[j] = ft_strcat(new_data[j], data[++i], ' ');
-            if (new_data[j][ft_strlen(new_data[j]) - 1] != '\"')
-                return (NULL);
-            j++;
-        }
-        else if(data[i][ft_strlen(data[i]) - 1] == '\"')
-            return (NULL);
-        else
-            new_data[j++] = ft_strdup(data[i]);
-    }
-    return (new_data[j] = '\0', new_data);
+	i = -1;
+	new_data = (char **)malloc(sizeof(char *) * (ft_matriz_len(data) + 1));
+	while (data[++i])
+	{
+		j = 0;
+		tmp = NULL;
+		data[i] = ft_strtrim(data[i], "\"");
+		while (data[i][j] && data[i][j] != '$')
+			j++;
+		if (data[i][j] == '$')
+		{
+			env_value = getenv(ft_substr(data[i], j + 1, ft_strlen(data[i]) - j
+						- 1));
+			if (env_value)
+				tmp = ft_strjoin(ft_substr(data[i], 0, j), env_value);
+			else
+				tmp = ft_strdup(data[i]);
+		}
+		else
+			tmp = ft_strdup(data[i]);
+		new_data[i] = tmp;
+	}
+	new_data[i] = '\0';
+	return (new_data);
 }
 
-char **ft_extended(char **data)
+char	**net_args(char *str)
 {
-    int i;
-    int j;
-    char **new_data;
-    char *tmp;
+	char **raw_data;
+	char **net_data;
+	char **data;
 
-    i = -1;
-    new_data = (char **)malloc(sizeof(char *) * (ft_matriz_len(data) + 1));
-    while (data[++i])
-    {
-        j = 0;
-        tmp = NULL; 
-        data[i] = ft_strtrim(data[i], "\""); 
-        while (data[i][j] && data[i][j] != '$')
-            j++;
-        if (data[i][j] == '$') 
-        {
-            char *env_value = getenv(ft_substr(data[i], j + 1, ft_strlen(data[i]) - j - 1));
-            if (env_value) 
-                tmp = ft_strjoin(ft_substr(data[i], 0, j), env_value);
-            else
-                tmp = ft_strdup(data[i]);
-        }
-        else
-            tmp = ft_strdup(data[i]);
-        new_data[i] = tmp;
-    }
-    new_data[i] = '\0';
-    return new_data;
-}
+	raw_data = ft_split(str, ' ');
+	net_data = ft_adjust_data(raw_data);
+	if (net_data == NULL)
+	{
+		write(1, "minishell: syntax error: quote\n", 31);
+		exit(1);
+	}
+	else
+	{
+		data = ft_extended(net_data);
+	}
 
-char **net_args(char *str)
-{
-    char **raw_data;
-    char **net_data;
-    char **data;
-
-    raw_data = ft_split(str, ' ');
-    net_data = ft_adjust_data(raw_data);
-    if(net_data == NULL)
-    {
-        write(1, "minishell: syntax error: quote\n", 31);
-        exit(1);
-    }
-    else
-    {
-        data = ft_extended(net_data);
-    }
-
-    for (int i = 0; raw_data[i]; i++)
-        free(raw_data[i]);
-    free(raw_data);
-
-    return data;
+	for (int i = 0; raw_data[i]; i++)
+		free(raw_data[i]);
+	free(raw_data);
+	return (data);
 }
