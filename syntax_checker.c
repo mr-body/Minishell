c753e99582469f4665636ab2630552165745001d
Gildo Komba <161140108+gkomba@users.noreply.github.com>
2024-11-05 15:34:15 +0100

    Update syntax_checker.c

--- syntax_checker.c
+++ syntax_checker.c
@@ -3,11 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   syntax_checker.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: waalexan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gkomba <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:35:50 by gkomba            #+#    #+#             */
-/*   Updated: 2024/11/04 22:11:29 by waalexan         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:19:40 by gkomba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	verify_pipes_syntax(char **matrix)
+{
+	int	i;
+
+	i = 0;
+	minishell->verify_pipes_syntax = ft_split_ms(minishell->command, '|');
+	while (matrix[i])
+	{
+		if (ft_is_only(matrix[i], ' '))
+		{
+			ft_putendl_fd("bash: syntax error near unexpected token `|'", 2);
+			ft_free_matrix(minishell->verify_pipes_syntax);
+			return (2);
+		}
+		i++;
+	}
+	ft_free_matrix(minishell->verify_pipes_syntax);
+	return (0);
+}
